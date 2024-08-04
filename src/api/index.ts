5591232420dc1b7da082943791223b680e6baa00
SonnyJim <newbey009@gmail.com>
2024-08-04 11:19:02 -0500

    add

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const defaultUrl = 'http://localhost:8080';
+const defaultUrl = 'http://10.0.2.2:8080';
 const imageUrl = `${defaultUrl}/file/resource/`;
 
 export const getEnv = () => {
