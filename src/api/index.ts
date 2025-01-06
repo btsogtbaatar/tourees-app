6776773a50ecaeaa744c5637a1dd5022659f8cd1
SonnyJim <newbey009@gmail.com>
2025-01-06 03:28:36 -0500

    double check

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
-const baseURL = '192.168.1.69:8080';
-// const baseURL = 'dv3427era6fzn.cloudfront.net';
-const defaultUrl = `http://${baseURL}`;
+// const baseURL = '192.168.1.69:8080';
+const baseURL = 'dv3427era6fzn.cloudfront.net';
+const defaultUrl = `https://${baseURL}`;
 const imageUrl = `${defaultUrl}/file/resource/`;
-const brokerUrl = `ws://${baseURL}/ws/websocket`;
+const brokerUrl = `wss://${baseURL}/ws/websocket`;
 
 export const getEnv = () => {
   return {
